499155e3b39de8e686b86987b6412e9eb6ccb545
Roman Calderon <romanc647@gmail.com>
2025-01-24 15:47:16 -0500

    Refactor Electron window creation to support development mode; load local server URL in development and HTML file in production.

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
+const isDev = process.env.NODE_ENV === 'development';
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: false,
     },
   });
 
-  win.loadFile("dist/index.html");
+  if (isDev) {
+    win.loadURL('http://localhost:9000');
+  } else {
+    win.loadFile("dist/index.html");
+  }
 }
 
 app.whenReady().then(createWindow);
